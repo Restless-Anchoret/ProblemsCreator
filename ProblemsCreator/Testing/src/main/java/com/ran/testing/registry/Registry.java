a6cd272299751362f70cac2c0ac7537fa0894692
RestlessAnchoret <et.anchoret@gmail.com>
2016-03-21 09:16:24 +0200

    Interface Registry and class AbstractRegistry were modified in Testing module.

--- ProblemsCreator/Testing/src/main/java/com/ran/testing/registry/Registry.java
+++ ProblemsCreator/Testing/src/main/java/com/ran/testing/registry/Registry.java
@@ -1,9 +1,12 @@
 package com.ran.testing.registry;
 
+import java.util.Collection;
+
 public interface Registry<T> {
 
     T getDefault();
     void put(String id, Supplier<T> supplier);
     T get(String id);
+    Collection<String> getAvailableIds();
     
 }
