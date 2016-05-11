03a648e3e23c6871c8c0af928cd37b82bca64b21
RestlessAnchoret <et.anchoret@gmail.com>
2016-05-11 13:33:03 +0200

    C++ support in Testing module was added (VisualCppLanguageToolkit.java). Decision time intercepting while time limit exceeding was added. Some other old classes in Testing module were modified.

--- ProblemsCreator/Testing/src/main/java/com/ran/testing/language/TimeLimitException.java
+++ ProblemsCreator/Testing/src/main/java/com/ran/testing/language/TimeLimitException.java
@@ -2,11 +2,26 @@ package com.ran.testing.language;
 
 public class TimeLimitException extends Exception {
 
+    private Integer decisionTime = null;
+    
     public TimeLimitException() {
     }
 
-    public TimeLimitException(String msg) {
-        super(msg);
+    public TimeLimitException(String message) {
+        super(message);
+    }
+    
+    public TimeLimitException(Integer decisionTime) {
+        this.decisionTime = decisionTime;
+    }
+    
+    public TimeLimitException(String message, Integer decisionTime) {
+        super(message);
+        this.decisionTime = decisionTime;
+    }
+
+    public Integer getDecisionTime() {
+        return decisionTime;
     }
 
 }
